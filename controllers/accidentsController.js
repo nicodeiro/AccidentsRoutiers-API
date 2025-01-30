55e3304e3cbede611489405750f1e5374f8b5720
nicodeiro <39009342@parisnanterre.fr>
2025-01-30 18:03:36 +0100

    Dataset Clean

--- controllers/accidentsController.js
+++ controllers/accidentsController.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 exports.getAccidents = async (req, res) => {
     try {
         const db = mongoose.connection.db;
-        const accidents = await db.collection('accidents_routiers').find().limit(100).toArray();
+        const accidents = await db.collection('accidents_routiers').find().limit(30000).toArray();
         res.json(accidents);
     } catch (err) {
         res.status(500).json({ error: err.message });
